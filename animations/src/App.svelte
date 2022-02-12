c89b9c7a42427514d8e4328fb9d0a39a8adde292
Morrianton1985 <morrianton@gmail.com>
2022-02-11 23:30:10 -0700

    added spring module and commented out tweened code.

--- animations/src/App.svelte
+++ animations/src/App.svelte
@@ -1,17 +1,19 @@
 <script>
-    import { writable } from "svelte/store";
-    import { tweened } from "svelte/motion";
-    import { cubicIn } from "svelte/easing";
+    // import { writable } from "svelte/store";
+    // import { tweened } from "svelte/motion";
+    // import { cubicIn } from "svelte/easing";
+    import Spring from "./Spring.svelte";
 
-    const progress = tweened(0, {
-        delay: 0,
-        duration: 700,
-        easing: cubicIn
-    });
+    // const progress = tweened(0, {
+    //     delay: 0,
+    //     duration: 700,
+    //     easing: cubicIn
+    // });
 
-    setTimeout(() => {
-        progress.set(0.5);
-    }, 1500);
+    // setTimeout(() => {
+    //     progress.set(0.5);
+    // }, 1500);
 </script>
 
-<progress value="{$progress}"></progress>
+<!-- <progress value="{$progress}"></progress> -->
+<Spring></Spring>
--- animations/src/Spring.svelte
+++ animations/src/Spring.svelte
@@ -1,5 +1,6 @@
 <script>
   import { writable } from "svelte/store";
+  import { spring } from "svelte/motion"  // only work with numbers and dates
   
   let cards = writable([
     {
@@ -19,7 +20,7 @@
       color: "orange"
     }
   ]);
-  let cardPos = writable([
+  let cardPos = spring([
     {
       rotation: 10,
       dx: 0
@@ -36,7 +37,11 @@
       rotation: -25,
       dx: 0
     }
-  ]);
+  ], {
+    stiffness: 0.1,  // value between 0 and 1
+    damping: 0.9,    // value between 0 and 1
+    precision: 0.001
+  });
 
   function discard(index) {
     cardPos.update(items => {
